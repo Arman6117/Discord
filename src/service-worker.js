2bce5a8be2c372240117b8bf0419e76f427861f5
Arman Patel <vanixgaming634@gmail.com>
2023-07-13 14:28:59 +0530

    Completed project

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,8 @@
 import { collection, doc, getDoc, getDocs } from "firebase/firestore";
 import { db } from "./firebase";
+import { registerRoute } from "workbox-routing";
+import { CacheFirst, NetworkFirst } from "workbox-strategies";
+import { precacheAndRoute } from "workbox-precaching";
 
 export const fetchServerDataForCache = async () => {
   try {
@@ -9,7 +12,7 @@ export const fetchServerDataForCache = async () => {
 
     for (const serverDoc of snapshot.docs) {
       const serverName = serverDoc.data().name;
-      const serverSubCollectionRef = doc(db, "servers", serverName);
+      const serverSubCollectionRef = doc(db, "servers", serverName, serverName);
       const serverSubCollectionDoc = await getDoc(serverSubCollectionRef);
 
       if (serverSubCollectionDoc.exists()) {
@@ -51,12 +54,48 @@ export const cacheServerIcons = async (serverIcons) => {
   }
 };
 
+// Fetch server data and cache icons
+this.addEventListener("install", (event) => {
+  event.waitUntil(
+    fetchServerDataForCache().then(({ serverData, serverIcons }) => {
+      // Cache server icons
+      cacheServerIcons(serverIcons);
 
-// Fetch server data and icons
-fetchServerDataForCache().then(({ serverData, serverIcons }) => {
-  // Cache server icons
-  cacheServerIcons(serverIcons);
+      // Use serverData as needed
 
-  // Use serverData as needed
-
+      // Skip waiting and activate the service worker immediately
+      this.skipWaiting();
+    })
+  );
 });
+
+// Cleanup old caches
+this.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => {
+            // Filter out caches that you want to keep
+          })
+          .map((cacheName) => {
+            return caches.delete(cacheName);
+          })
+      );
+    })
+  );
+});
+
+// Precache and route the assets using Workbox
+precacheAndRoute(this.__WB_MANIFEST);
+
+// Configure caching strategies for specific routes
+registerRoute(
+  // Cache strategy for server icons
+  ({ url }) => {
+    return url.pathname.includes("/servers/") && url.pathname.endsWith("/image.jpg");
+  },
+  new CacheFirst()
+);
+
+registerRoute(new NetworkFirst());
